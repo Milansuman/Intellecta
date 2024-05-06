daeecb52585043d7a6adac8f5a50264c1a9dc3ba
Milansuman <milansuman2016@gmail.com>
2024-05-06 11:51:12 +0530

    fix error handling in login

--- app/groups/[title]/page.tsx
+++ app/groups/[title]/page.tsx
@@ -30,7 +30,7 @@ export default async function PeerGroup({params}: {params: {title : string}}){
                     <h1 className="text-xl font-bold">{peerGroup.name as string}</h1>
                     <Image src={peerGroup.image as string} width={400} height={400} alt={peerGroup.name as string} className="w-96 h-64 rounded-md"/>
                     <p>{peerGroup.description as string}</p>
-                    <div className="flex flex-row gap-3">
+                    <div className="flex flex-row gap-3 max-w-96 flex-wrap">
                         {
                             users.map(user => (
                                 <Badge className="w-fit h-fit rounded-md" key={user.email}>{user.email}</Badge>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,30 +1,29 @@
+"use client";
+
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
+import {Toaster} from "@/components/ui/sonner"
+import { toast } from "sonner"
 
 import Link from "next/link"
 
-import { login } from "@/lib/db"
-import { cookies } from "next/headers"
-import { redirect } from "next/navigation"
+import { loginHandler } from "./actions"
+import { useFormState } from "react-dom";
+import { useEffect} from "react";
 
 export default function Login(){
-    async function loginHandler(formData: FormData){
-        'use server';
+    const [state, formAction] = useFormState(loginHandler, { error: false })
 
-        try {
-            const token = await login(formData.get("email") as string, formData.get("password") as string);
-            cookies().set("session", token);
-            
-        } catch (error) {
-            throw error;
+    useEffect(() => {
+        if(state.error){
+            toast.error("An error occurred")
         }
-        redirect("/");
-    }
+    }, [state])    
 
     return (
         <main className="flex flex-col h-full justify-center items-center">
-            <form method="POST" action={loginHandler} className="flex flex-col gap-4 shadow-md rounded-md p-6 bg-white min-w-96">
+            <form action={formAction} className="flex flex-col gap-4 shadow-md rounded-md p-6 bg-white min-w-96">
                 <h1 className="font-bold text-3xl">Login</h1>
                 <div className="flex flex-col gap-2">
                     <Label>Email</Label>
@@ -37,6 +36,7 @@ export default function Login(){
                     </Link>
                 </div>
             </form>
+            <Toaster/>
         </main>
     )
 }
