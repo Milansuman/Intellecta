8eeec769577e7b249b5310c1effacbacf9ed0c55
Milansuman <milansuman2016@gmail.com>
2024-05-06 18:16:13 +0530

    clear cache during upload

--- app/resources/actions.ts
+++ app/resources/actions.ts
@@ -2,8 +2,10 @@
 
 import { addResource } from "@/lib/db";
 import {redirect} from "next/navigation"
+import { revalidatePath } from "next/cache";
 
 export async function resourceUploadHandler(formData: FormData){
     await addResource(formData.get("resource") as File, (formData.get("tags") as string).split(",").map(tag => tag.trim()))
+    revalidatePath("/resources");
     redirect("/resources");
 }
